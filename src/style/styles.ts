d0a746747efc64304350f3455f2d90737ac838af
Thales Minoru Sato <thalesms2@outlook.com>
2022-04-29 16:57:09 -0300

    Update styles.ts
    mobile adaptation

--- src/style/styles.ts
+++ src/style/styles.ts
@@ -4,8 +4,6 @@ export const mainColor = "#D6000D"
 
 export const Header = styled.section`
     display: flex;
-    flex-direction: row;
-    justify-content: space-between;
     align-items: center;
     padding: 0 3em;
     min-height: 100px;
@@ -16,17 +14,31 @@ export const Header = styled.section`
             color: black;
         }
     }
-
     div {
         display: flex;
         flex-direction: row;
         align-items: center;
     }
-
     a {
         color: black;
-        margin-right: 3vw;
         font-size: 1.1em;
+    }
+    @media (min-width:800px) {
+        justify-content: space-between;
+        flex-direction: row;
+        a {
+            margin-right: 3vw;
+        }
+    }
+    @media (max-width: 800px) {
+        justify-content: center;
+        flex-direction: column;
+        Button {
+            display: none;
+        }
+        a:nth-child(1), a:nth-child(2) {
+            margin-right: 10vw;
+        }
     }
 `
 
@@ -37,17 +49,20 @@ export const Services = styled.section`
         max-width: 500px;
         max-height: 400px;
     }
-    margin: 10em 0;
-    padding: 0 20vw;
+    @media (min-width:800px) {
+        padding: 0 20vw;
+        margin: 10em 0;
+    }
     @media(max-width: 800px) {
-        flex-direction: row;
+        flex-direction: column;
+        margin: 4em 0;
+        padding: 0 10vw;
     }
 `
 
 export const TextDiv = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: center;
     span:nth-child(1) {
         font-size: 1.2em;
         font-weight: bold;
@@ -56,15 +71,36 @@ export const TextDiv = styled.div`
         color: ${mainColor};
         font-size: 3em;
         font-weight: 900;
-        margin-bottom: .8em;
     }
     Button {
         max-width: 250px;
     }
+    @media (min-width:800px) {
+        justify-content: center;
+        span:nth-child(2) {
+            margin-bottom: .8em;
+        }
+    }
+    @media(max-width: 800px) {
+        align-items: center;
+        padding-top: 2em;
+        span:nth-child(2) {
+            margin-bottom: .5em;
+        }
+    }
 `
 
 export const ListDiv = styled.div`
-    padding-top: 110px;
+    @media (min-width: 800px) {
+        padding-top: 110px;
+    }
+    @media (max-width: 800px) {
+        padding-top: 30px;
+        order: 1;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 `
 
 export const List = styled.ul`
@@ -88,7 +124,6 @@ export const List = styled.ul`
 export const Contact = styled.section`
     display: flex;
     flex-direction: column;
-    margin-top: 60px;
     form {
         padding: .5em 0;
     }
@@ -96,23 +131,48 @@ export const Contact = styled.section`
         background-color: #BABABA;
         padding: 1em;
         border-radius: .5em;
-        margin-right: .5em;
-        min-width: 250px;
     }
+    @media (min-width: 800px) {
+        margin-top: 60px;
+        input {
+            margin-right: .5em;
+            min-width: 250px;
+        }
+    }
+    @media (max-width: 800px) {
+        margin-top: 30px;
+        justify-content: center;
+        input {
+            margin-bottom: .5em;
+        }
+        form {
+            display: flex;
+            flex-direction: column;
+        }
+    }
+
 `
 
 export const Comments = styled.section`
     display: flex;
-    justify-content: space-around;
-    padding: 0 5em;
+    @media (min-width: 800px) {
+        justify-content: space-around;
+        padding: 0 5em;
+    }
+    @media (max-width: 800px) {
+        flex-direction: column;
+        align-items: center;
+        padding: 0 1em;
+    }
 `
 
 export const Card = styled.div`
     display: flex;
-
+    border-radius: 1em;
     background-color: #BABABA;
     padding: 1em;
     width: 600px;
+    margin: 1em;
     div:nth-child(1) {
         width: 80px;
         height: 80px;
@@ -146,9 +206,7 @@ export const Footer = styled.section`
     margin-top: 5em;
     color: white;
     display: flex;
-    flex-direction: row;
     padding: 3em;
-    justify-content: space-around;
     div {
         display: flex;
         flex-direction: column;
@@ -170,6 +228,18 @@ export const Footer = styled.section`
             fill: ${mainColor};
         }
     }
+    @media (min-width: 800px) {
+        flex-direction: row;
+        justify-content: space-around;
+    }
+    @media (max-width: 800px) {
+        flex-direction: column;
+        div:nth-child(1) {
+            align-self: center;
+            align-items: center;
+        }
+    }
+
 `
 
 export const IconsDiv = styled.div`
